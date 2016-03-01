181a0ba7ebcf1546648c971070b1912a12978407
calebgregory <calebgregory@gmail.com>
2016-02-29 18:54:11 -0600

    Adding attack route

--- app/routes/ctrl/src/attack.js
+++ app/routes/ctrl/src/attack.js
@@ -1,3 +1,10 @@
 'use strict'
 
-module.exports = (req, res) => res.status(200).send('a-ok!')
+module.exports = (req, res) => {
+  const { attacker, attackee } = req.query
+  if(!(attacker && attackee)) {
+    return res.status(417).send(new Error('No attackee or attacker sent!'))
+  }
+
+  return res.status(200).json({ 'abc': '', '123': '' })
+}
--- test/routes/attack/post.js
+++ test/routes/attack/post.js
@@ -10,26 +10,45 @@ module.exports = {
       .end(done)
   },
 
+  '417_attacker': (done) => {
+    request
+      .post('/attack')
+      .query({ attackee: '123' })
+      .expect(417)
+      .end(done)
+  },
+
+  '417_attackee': (done) => {
+    request
+      .post('/attack')
+      .query({ attacker: 'abc' })
+      .expect(417)
+      .end(done)
+  },
+
   200: (done) => {
     request
-      .post('/attack', { attacker: 'abc', attackee: '123' })
+      .post('/attack')
+      .query({ attacker: 'abc', attackee: '123' })
       .expect(200)
       .end(done)
   },
 
   json: (done) => {
     request
-      .post('/attack', { attacker: 'abc', attackee: '123' })
+      .post('/attack')
+      .query({ attacker: 'abc', attackee: '123' })
       .expect('Content-Type', /json/)
       .end(done)
   },
 
   newState: (done) => {
     request
-      .post('/attack', { attacker: 'abc', attackee: '123' })
+      .post('/attack')
+      .query({ attacker: 'abc', attackee: '123' })
       .end((err, res) => {
-        expect(res).to.have.property('abc')
-        expect(res).to.have.property('123')
+        expect(res.body).to.have.property('abc')
+        expect(res.body).to.have.property('123')
         done()
       })
   }
